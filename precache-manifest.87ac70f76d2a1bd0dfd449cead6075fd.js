0bc053b55f34ae77964e15754957b363572ed7dd
Fatma Ashraf <fatma_ashraf12@yahoo.com>
2020-10-15 16:42:46 +0200

    Updates

---
+++ precache-manifest.87ac70f76d2a1bd0dfd449cead6075fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63a55d37f3d5d59ce63f1ecb6e962f0",
+    "url": "/ReactTemp/index.html"
+  },
+  {
+    "revision": "c44996b816f656c28eef",
+    "url": "/ReactTemp/static/css/2.b14f1a68.chunk.css"
+  },
+  {
+    "revision": "00b3d5d9a2f11f700cc1",
+    "url": "/ReactTemp/static/css/main.761d30d4.chunk.css"
+  },
+  {
+    "revision": "c44996b816f656c28eef",
+    "url": "/ReactTemp/static/js/2.5ba5af59.chunk.js"
+  },
+  {
+    "revision": "2a465d7c85b3632f78913a3aa4d0ba49",
+    "url": "/ReactTemp/static/js/2.5ba5af59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b3d5d9a2f11f700cc1",
+    "url": "/ReactTemp/static/js/main.4a44e351.chunk.js"
+  },
+  {
+    "revision": "55ddb4298667a8646a5c",
+    "url": "/ReactTemp/static/js/runtime-main.344b042d.js"
+  }
+]);
